0236e306b7bb9173f757477f9c1f50f8d2ef1a3e
hiroyuki.nishi <hiroyuki.nishi@motex.co.jp>
2021-07-23 16:38:16 +0900

    material用のボタンを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,14 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
-// class Detail extends React.Component {
-//   render() {
-//     return (
-//       <div >
-//         <div className="classification-name">名前</div>
-//         <div className="description">説明</div>
-//         <div className="unit-price">0円</div>
-//         <div className="num-people">
-//           <select value="0">
-//             <option value="0">0</option>
-//             <option value="1">1</option>
-//             <option value="2">2</option>
-//             <option value="3">3</option>
-//             <option value="4">4</option>
-//           </select>
-//           <span>名</span>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// class Summary extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <div className="party">
-//           <input type="text" className="party" value="0" />
-//           <span>名様</span>
-//         </div>
-//         <div className="total-amount">
-//           <span>合計</span>
-//           <input type="text" className="total-amount" value="0" />
-//           <span>円</span>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 class Hoge extends React.Component {
   render() {
     return (
-      <h1>Hoge</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     )
   }
 }
