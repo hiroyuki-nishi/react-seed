8ad8c450f969eff6109cd0e3c3bb78a5adbac832
hiroyuki.nishi <hiroyuki.nishi@motex.co.jp>
2021-07-23 19:21:48 +0900

    Input改良

--- src/common/input/MaterialInput.tsx
+++ src/common/input/MaterialInput.tsx
@@ -6,14 +6,31 @@ import React from 'react';
 interface MaterialInputProps {
   label: string;
   variant: "standard" | "filled" | "outlined" | undefined;
+  changeValue: (value: string) => void
 }
 
 export class MaterialInput extends React.Component<MaterialInputProps> {
+  // constructor(props: MaterialInputProps) {
+  //   super(props);
+  //   // this.state = {value: ""};
+  // }
+
+  handleChange(): any {
+    return (event: any) => {
+      // this.setState({value: event.target.value});
+      this.props.changeValue(event.target.value);
+    }
+  }
+
   render() {
     return (
       <>
         <form noValidate autoComplete="off">
-          <TextField label={ this.props.label } variant={ this.props.variant }/>
+          <TextField
+            label={this.props.label}
+            variant={this.props.variant}
+            onChange={this.handleChange()}
+          />
         </form>
       </>
     );
--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { MaterialInput } from '../common/input/MaterialInput';
 
+
+// interface LoginProps {
+//   value: string;
+// }
+
 export class Login extends React.Component {
+  changeValue(value: string): void {
+    console.log(value)
+    this.setState({ value: value });
+  }
+
   render() {
     return (
       <>
         <form noValidate autoComplete="off">
-          <MaterialInput label="aa" variant="standard" />
+          <MaterialInput label="email" variant="standard" changeValue={this.changeValue.bind(this)}/>
         </form>
       </>
     );
